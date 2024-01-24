f0e2d8b8745e206f26089f532689149c3b92ed7d
krok-code <maks.smolinskyi@gmail.com>
2024-01-24 22:32:55 +0200

    Update FormAddContact.jsx

--- src/components/FormAddContact.jsx
+++ src/components/FormAddContact.jsx
@@ -1,74 +1,72 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
+const FormAddContact = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-export class FormAddContact extends Component {
-  state = {
-    ...INITIAL_STATE,
-  };
-
-  handleInputChange = e => {
+  const handleInputChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    if (name === 'name') {
+      setName(value);
+    }
+    if (name === 'number') {
+      setNumber(value);
+    }
+    console.warn(`no field with name ${name}`);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ name: this.state.name, number: this.state.number });
-    this.setState({ ...INITIAL_STATE });
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <Form
-        onSubmit={this.handleSubmit}
-        className="border border-primary-subtle p-3 rounded-3"
-      >
-        <Form.Group className="mb-3" controlId="formAddContactName">
-          <Form.Label className="fs-4 fw-medium">Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            required
-            placeholder="Enter contact name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-          <Form.Text className="text-muted">
-            Name may contain only letters, apostrophe, dash and spaces. For
-            example Adrian, Jacob Mercer, Charles de Batz de Castelmore
-            d'Artagnan
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formAddContactTel">
-          <Form.Label className="fs-4 fw-medium">Number</Form.Label>
-          <Form.Control
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            required
-            placeholder="Enter contact phone number"
-            value={this.state.number}
-            onChange={this.handleInputChange}
-          />
-          <Form.Text className="text-muted">
-            Phone number must be digits and can contain spaces dashes,
-            parentheses and can start with +
-          </Form.Text>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Add contact
-        </Button>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      className="border border-primary-subtle p-3 rounded-3"
+    >
+      <Form.Group className="mb-3" controlId="formAddContactName">
+        <Form.Label className="fs-4 fw-medium">Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="name"
+          pattern="^[a-zA-Z\u0400-\u04FF]+(([' -][a-zA-Z\u0400-\u04FF ])?[a-zA-Z\u0400-\u04FF]*)*$"
+          required
+          placeholder="Enter contact name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <Form.Text className="text-muted">
+          Name may contain only letters, apostrophe, dash and spaces. For
+          example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formAddContactTel">
+        <Form.Label className="fs-4 fw-medium">Number</Form.Label>
+        <Form.Control
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          required
+          placeholder="Enter contact phone number"
+          value={number}
+          onChange={handleInputChange}
+        />
+        <Form.Text className="text-muted">
+          Phone number must be digits and can contain spaces dashes, parentheses
+          and can start with +
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Add contact
+      </Button>
+    </Form>
+  );
+};
 
 FormAddContact.propTypes = {
   onSubmit: PropTypes.func.isRequired,
