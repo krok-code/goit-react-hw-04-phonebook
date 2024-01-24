6b706e7b506836d3a09386b3eed1cd67689d6073
krok-code <maks.smolinskyi@gmail.com>
2024-01-24 22:22:00 +0200

    refactor: change proptypes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,6 @@ Contact.propTypes = {
     name: PropTypes.string.isRequired,
     number: PropTypes.string.isRequired,
   }),
-  onDelete: PropTypes.func.isRequired,
 };
 
 export default Contact;
